2eba4ff56deda17d33446c86b81776530d8b8a51
godspeedcurry <3160100421@zju.edu.cn>
2018-02-23 11:33:29 +0800

    read and print

--- 1.1.py
+++ 1.1.py
@@ -4,4 +4,8 @@ import numpy as np
 import cv2
 
 img = cv2.imread('test.png', 0)
-print img
+img = cv2.imread('test.png', 'COLOR')
+img1 = cv2.imread_GRAYSCALE('test.png', 0)
+img2 = cv2.imread_UNCHANGED('test.png', 0)
+
+print img, img1, img2
